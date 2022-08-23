8868597e193b3e34db215731bc3e337a55be94ad
lucas_avila_96 <56192229+lucas-avila-96@users.noreply.github.com>
2022-08-23 08:19:56 -0300

    Add files via upload

---
+++ Ejercicio6/main.py
@@ -0,0 +1,19 @@
+
+from src.classQueue import Queue
+
+
+
+if __name__ == '__main__':
+    cola = Queue()
+
+    cola.enqueue('a')
+    cola.enqueue('b')
+    cola.enqueue('c')
+    cola.enqueue('d')
+    cola.enqueue('e')
+
+
+    cola.dequeue()
+    cola.dequeue()
+
+    print(cola)
